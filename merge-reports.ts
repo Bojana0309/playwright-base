f53ff1a5817efbe1e2c09b7069358f1bf6067a96
Bojana0309 <b.antonic@levi9.com>
2023-07-10 16:16:22 +0200

    change sharding matrix

--- merge-reports.ts
+++ merge-reports.ts
@@ -1,15 +1,12 @@
 import { mergeHTMLReports } from 'playwright-merge-html-reports';
 
 const inputReportPaths = [
-  process.cwd() + '/playwright-report-chromium-1_3',
-  process.cwd() + '/playwright-report-chromium-2_3',
-  process.cwd() + '/playwright-report-chromium-3_3',
-  process.cwd() + '/playwright-report-firefox-1_3',
-  process.cwd() + '/playwright-report-firefox-2_3',
-  process.cwd() + '/playwright-report-firefox-3_3',
-  process.cwd() + '/playwright-report-webkit-1_3',
-  process.cwd() + '/playwright-report-webkit-2_3',
-  process.cwd() + '/playwright-report-webkit-3_3'
+  process.cwd() + '/playwright-report-chromium-1_2',
+  process.cwd() + '/playwright-report-chromium-2_2',
+  process.cwd() + '/playwright-report-firefox-1_2',
+  process.cwd() + '/playwright-report-firefox-2_2',
+  process.cwd() + '/playwright-report-webkit-1_2',
+  process.cwd() + '/playwright-report-webkit-2_2'
 ];
 
 const config = {
