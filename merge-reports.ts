5387850500bea152d42c672b10d1a04c5f6ddb00
Bojana0309 <b.antonic@levi9.com>
2023-07-12 15:22:00 +0200

    change sharding matrix 2

--- merge-reports.ts
+++ merge-reports.ts
@@ -1,14 +1,22 @@
 import { mergeHTMLReports } from 'playwright-merge-html-reports';
 
 const inputReportPaths = [
-  process.cwd() + '/playwright-report-api-tests-1_2',
-  process.cwd() + '/playwright-report-api-tests-2_2',
-  process.cwd() + '/playwright-report-e2e-tests-chromium-1_2',
-  process.cwd() + '/playwright-report-e2e-tests-chromium-2_2',
-  process.cwd() + '/playwright-report-e2e-tests-firefox-1_2',
-  process.cwd() + '/playwright-report-e2e-tests-firefox-2_2',
-  process.cwd() + '/playwright-report-e2e-tests-webkit-1_2',
-  process.cwd() + '/playwright-report-e2e-tests-webkit-2_2'
+  process.cwd() + '/playwright-report-api-tests-1_4',
+  process.cwd() + '/playwright-report-api-tests-2_4',
+  process.cwd() + '/playwright-report-api-tests-3_4',
+  process.cwd() + '/playwright-report-api-tests-4_4',
+  process.cwd() + '/playwright-report-e2e-tests-chromium-1_4',
+  process.cwd() + '/playwright-report-e2e-tests-chromium-2_4',
+  process.cwd() + '/playwright-report-e2e-tests-chromium-3_4',
+  process.cwd() + '/playwright-report-e2e-tests-chromium-4_4',
+  process.cwd() + '/playwright-report-e2e-tests-firefox-1_4',
+  process.cwd() + '/playwright-report-e2e-tests-firefox-2_4',
+  process.cwd() + '/playwright-report-e2e-tests-firefox-3_4',
+  process.cwd() + '/playwright-report-e2e-tests-firefox-4_4',
+  process.cwd() + '/playwright-report-e2e-tests-webkit-1_4',
+  process.cwd() + '/playwright-report-e2e-tests-webkit-2_4',
+  process.cwd() + '/playwright-report-e2e-tests-webkit-3_4',
+  process.cwd() + '/playwright-report-e2e-tests-webkit-4_4'
 ];
 
 const config = {
