3e3256343b8f5c922166cd9788a60093d5ac1849
Bojana0309 <b.antonic@levi9.com>
2023-07-12 16:11:31 +0200

    change sharding matrix 4

--- merge-reports.ts
+++ merge-reports.ts
@@ -1,26 +1,18 @@
 import { mergeHTMLReports } from 'playwright-merge-html-reports';
 
 const inputReportPaths = [
-  process.cwd() + '/playwright-report-api-tests-1_5',
-  process.cwd() + '/playwright-report-api-tests-2_5',
-  process.cwd() + '/playwright-report-api-tests-3_5',
-  process.cwd() + '/playwright-report-api-tests-4_5',
-  process.cwd() + '/playwright-report-api-tests-5_5',
-  process.cwd() + '/playwright-report-e2e-tests-chromium-1_5',
-  process.cwd() + '/playwright-report-e2e-tests-chromium-2_5',
-  process.cwd() + '/playwright-report-e2e-tests-chromium-3_5',
-  process.cwd() + '/playwright-report-e2e-tests-chromium-4_5',
-  process.cwd() + '/playwright-report-e2e-tests-chromium-5_5',
-  process.cwd() + '/playwright-report-e2e-tests-firefox-1_5',
-  process.cwd() + '/playwright-report-e2e-tests-firefox-2_5',
-  process.cwd() + '/playwright-report-e2e-tests-firefox-3_5',
-  process.cwd() + '/playwright-report-e2e-tests-firefox-4_5',
-  process.cwd() + '/playwright-report-e2e-tests-firefox-5_5',
-  process.cwd() + '/playwright-report-e2e-tests-webkit-1_5',
-  process.cwd() + '/playwright-report-e2e-tests-webkit-2_5',
-  process.cwd() + '/playwright-report-e2e-tests-webkit-3_5',
-  process.cwd() + '/playwright-report-e2e-tests-webkit-4_5',
-  process.cwd() + '/playwright-report-e2e-tests-webkit-5_5'
+  process.cwd() + '/playwright-report-api-tests-1_3',
+  process.cwd() + '/playwright-report-api-tests-2_3',
+  process.cwd() + '/playwright-report-api-tests-3_3',
+  process.cwd() + '/playwright-report-e2e-tests-chromium-1_3',
+  process.cwd() + '/playwright-report-e2e-tests-chromium-2_3',
+  process.cwd() + '/playwright-report-e2e-tests-chromium-3_3',
+  process.cwd() + '/playwright-report-e2e-tests-firefox-1_3',
+  process.cwd() + '/playwright-report-e2e-tests-firefox-2_3',
+  process.cwd() + '/playwright-report-e2e-tests-firefox-3_3',
+  process.cwd() + '/playwright-report-e2e-tests-webkit-1_3',
+  process.cwd() + '/playwright-report-e2e-tests-webkit-2_3',
+  process.cwd() + '/playwright-report-e2e-tests-webkit-3_3'
 ];
 
 const config = {
