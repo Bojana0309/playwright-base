1cdacba687d990b678e2dd4e9776acb41be79db8
Bojana0309 <b.antonic@levi9.com>
2023-07-12 13:06:58 +0200

    fix report paths

--- merge-reports.ts
+++ merge-reports.ts
@@ -3,12 +3,12 @@ import { mergeHTMLReports } from 'playwright-merge-html-reports';
 const inputReportPaths = [
   process.cwd() + '/playwright-report-api-tests-1_2',
   process.cwd() + '/playwright-report-api-tests-2_2',
-  process.cwd() + '/playwright-report-chromium-1_2',
-  process.cwd() + '/playwright-report-chromium-2_2',
-  process.cwd() + '/playwright-report-firefox-1_2',
-  process.cwd() + '/playwright-report-firefox-2_2',
-  process.cwd() + '/playwright-report-webkit-1_2',
-  process.cwd() + '/playwright-report-webkit-2_2'
+  process.cwd() + '/playwright-report-e2e-tests-chromium-1_2',
+  process.cwd() + '/playwright-report-e2e-tests-chromium-2_2',
+  process.cwd() + '/playwright-report-e2e-tests-firefox-1_2',
+  process.cwd() + '/playwright-report-e2e-tests-firefox-2_2',
+  process.cwd() + '/playwright-report-e2e-tests-webkit-1_2',
+  process.cwd() + '/playwright-report-e2e-tests-webkit-2_2'
 ];
 
 const config = {
